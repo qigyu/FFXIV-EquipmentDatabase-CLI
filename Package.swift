ad5702b1d0b6a2a8d35a3c6f5cc45fca1daa7776
dodocha169 <dodocha1524@icloud.com>
2021-07-07 15:16:28 +0900

    fix package.swift and generate xcodeproj

--- Package.swift
+++ Package.swift
@@ -6,18 +6,6 @@ import PackageDescription
 let package = Package(
     name: "FFXIV-EquipmentDatabase-CLI",
     dependencies: [
-       .package(name: "Tokamak",
-                 url: "https://github.com/TokamakUI/Tokamak",
-                 Version(0,6,1) ..< Version(0,6,4)),
-        .package(name: "TokamakHTMLFunctionBuilder",
-                 url:"https://github.com/evdwarf/tokamak-html-function-builder",
-                 from: "0.0.4"),
-        .package(name: "Closswift",
-                 url: "https://github.com/evdwarf/closswift",
-                 from: "0.0.1"),
-        .package(name: "TailwindSwift",
-                 url: "https://github.com/evdwarf/tailwind-swift",
-                 from: "0.1.0"),
         .package(name: "Commander",
                  url: "https://github.com/kylef/Commander.git",
                  from: "0.9.1"),
@@ -27,11 +15,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FFXIV-EquipmentDatabase-CLI",
-            dependencies: [.product(name: "TokamakShim", package: "Tokamak"),
-                "TokamakHTMLFunctionBuilder",
-                "Closswift",
-                "TailwindSwift",
-                "Commander"]),
+            dependencies: [ "Commander",]),
         .testTarget(
             name: "FFXIV-EquipmentDatabase-CLITests",
             dependencies: ["FFXIV-EquipmentDatabase-CLI"]),
